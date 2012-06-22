9888f980c2c60328f64ff52a0336a4eefc99d646
Joe LAngston <joe@deuce>
2012-06-22 14:27:52 -0400

    Fix script for 32-bit build
    Updated apps

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
+rm -rf sdk/emulator
 rm -rf external/qem*
 rm out/target/product/ace/system/build.prop
 
@@ -11,7 +12,7 @@ export CCACHE_DIR=/$HOME/.ccache
 prebuilt/linux-x86/ccache/ccache -M 40G
 
 
-make -j1 lord
+make -j2 lord
 
 #build on new linux
 # make CC=gcc-4.4 CXX=g++-4.4 lord -j6
